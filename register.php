64af2750d6870ede38b60605ae18b2fb9a364a82
Evandro Fonseca Junior <51557722+truuta@users.noreply.github.com>
2022-04-22 15:44:24 -0300

    Add files via upload

---
+++ register.php
@@ -0,0 +1,32 @@
+<?php
+include "configs.js";
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'users';
+
+$email = $_POST['email_name'];
+$senha = $_POST['senha_name'];
+$nome = $_POST['nome_name'];
+$sobrenome = $_POST['sobrenome_name'];
+$rg = $_POST['rg_name'];
+$cpf = $_POST['cpf_name'];
+
+$mysqli = new mysqli($host, $user, $password, $database );
+
+if ( !$mysqli ) {
+    die( 'Connection failed: ' . mysqli_connect_error() );
+}
+
+$sql = "INSERT INTO dados (email, senha, nome, sobrenome, rg, cpf) VALUES ('$email', '$senha', '$nome', '$sobrenome', '$rg', '$cpf')";
+if ( mysqli_query( $mysqli, $sql ) ) {
+header('Location: /projectone/login.php');
+die();
+}
+else {
+    echo 'Error: ' . $sql . '<br>' . mysqli_error($mysqli);
+}
+
+mysqli_close( $mysqli );
+?>
